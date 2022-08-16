a0faa9dec76b067503804670b21c74fb2ceac028
Gavin Morrice <gavin@gavinmorrice.com>
2022-08-16 16:36:24 +0100

    Remove redundant controller

--- app/controllers/recipes_controller.rb
+++
@@ -1,9 +0,0 @@
-class RecipesController < ApplicationController
-  def cached
-    render :index, assigns: { user: User.find(params[:user_id]) }
-  end
-
-  def memoized
-    render :index, assigns: { user: User.find(params[:user_id]) }
-  end
-end
